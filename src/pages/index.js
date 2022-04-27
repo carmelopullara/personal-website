6c32cc33669d9877301413f4f836343da6dc45c4
Carmelo Pullara <carmelo@pullara.me>
2022-04-27 17:57:26 +0200

    Update page

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,8 +4,6 @@ import 'normalize.css/normalize.css';
 import Loading from '../components/Loading';
 import Navbar from '../components/Navbar';
 import Home from '../components/Home';
-import About from '../components/About';
-import Works from '../components/Works';
 import Footer from '../components/Footer';
 import GlobalStyle from '../styled/GlobalStyle';
 
@@ -29,8 +27,6 @@ export default () => (
     <Loading />
     <Navbar />
     <Home />
-    <About />
-    <Works />
     <Footer />
   </Fragment>
 );
